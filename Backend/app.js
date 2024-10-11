49a2ea23bb47f3f01c9a2b4a3c7373e5cc1f5d0a
Priyanshubham7 <priyanshu.srivastava@mckinleyrice.co>
2024-10-11 16:56:24 +0530

    worked on models and controller

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,27 @@
 import express from "express";
+import cors from "cors";
+import { errorHandler, notFound } from "./Middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/auth.route.js";
+import productRoute from "./routes/product.route.js";
 
 const app = express();
+
+//CORS
+app.use(cors());
 
 //JSON BODY
 app.use(express.json());
 
-//CORS
-app.use(cors());
+//cokkie-parser
+app.use(cookieParser());
+
+// ROUTES
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/products", productRoute);
+
+//Error middleware
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
