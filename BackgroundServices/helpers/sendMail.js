120eb9fe89e076cff6ef48785dd6dc8129540bc8
Priyanshubham7 <priyanshu.srivastava@mckinleyrice.co>
2024-10-18 20:30:31 +0530

    Added email services

--- BackgroundServices/helpers/sendMail.js
+++ BackgroundServices/helpers/sendMail.js
@@ -1,28 +1,25 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 function createTransporter(config) {
-  const transporter = nodemailer.createTransporter(config);
+  const transporter = nodemailer.createTransport(config);
   return transporter;
 }
-
 let configurations = {
   service: "gmail",
   host: "smtp.gmail.com",
   port: 587,
-  requireTls: true,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL,
-    password: process.env.PASSWORD,
+    pass: process.env.PASSWORD,
   },
 };
 
 const sendMail = async (messageoption) => {
   const transporter = await createTransporter(configurations);
   await transporter.verify();
-
   await transporter.sendMail(messageoption, (error, info) => {
     if (error) {
       console.log(error);
