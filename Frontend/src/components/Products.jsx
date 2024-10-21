78c4e100e33486e1b9c56e42bc905a1204b8052b
Priyanshubham7 <priyanshu.srivastava@mckinleyrice.co>
2024-10-22 00:56:50 +0530

    Working on products page in Frontend

---
+++ Frontend/src/components/Products.jsx
@@ -0,0 +1,16 @@
+import Product from "./Product";
+
+function Products() {
+  return (
+    <div className="flex flex-wrap mx-[30px]">
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
+
+export default Products;
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Banner from "../components/Banner"
+import Category from "../components/Category"
+import Products from "../components/Products"
+function Home() {
+  return (
+    <div>
+      <Banner />
+      <Category />
+      <Products />
+    </div>
+  )
+}
+
+export default Home
